8776b24b421a9762d9346e128a05d88878594d75
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2023-01-05 11:04:20 +0100

    modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,6 @@ import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
 import { Loader } from './Loader/Loader';
 import { Modal } from 'components/Modal/Modal';
-
 export class App extends Component {
   state = {
     subject: [],
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,49 +1,90 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import css from './Modal.module.css';
 import PropTypes from 'prop-types';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    // console.log('Modal componentDidMount');
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    // console.log('Modal componentWillUnmount');
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
-    // console.log(e.code);
+export const Modal = ({ img, closeModal }) => {
+  const handleKeyDown = e => {
     if (e.code === 'Escape') {
-      // console.log('Нажали Esc и я закрываю модалку');
-      this.props.closeModal();
+      closeModal();
     }
   };
 
-  handleBackdropClick = e => {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
+
+  const handleBackdropClick = e => {
     if (e.currentTarget === e.target) {
-      // console.log('Кликнули в бэкдроп');
-      this.props.closeModal();
+      closeModal();
     }
   };
 
-  render() {
-    const { img } = this.props;
-    return createPortal(
-      <div className={css.modal_backdrop} onClick={this.handleBackdropClick}>
-        <div className={css.modal_content}>
-          <img src={img} alt="" />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <div className={css.modal_backdrop} onClick={handleBackdropClick}>
+      <div className={css.modal_content}>
+        <img src={img} alt="" />
+      </div>
+    </div>,
+    modalRoot
+  );
+};
 
 Modal.propTypes = {
   img: PropTypes.string.isRequired,
 };
+
+// import React, { Component } from 'react';
+// import { createPortal } from 'react-dom';
+// import css from './Modal.module.css';
+// import PropTypes from 'prop-types';
+
+// const modalRoot = document.querySelector('#modal-root');
+
+// export class Modal extends Component {
+//   componentDidMount() {
+//     // console.log('Modal componentDidMount');
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     // console.log('Modal componentWillUnmount');
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = e => {
+//     // console.log(e.code);
+//     if (e.code === 'Escape') {
+//       // console.log('Нажали Esc и я закрываю модалку');
+//       this.props.closeModal();
+//     }
+//   };
+
+//   handleBackdropClick = e => {
+//     if (e.currentTarget === e.target) {
+//       // console.log('Кликнули в бэкдроп');
+//       this.props.closeModal();
+//     }
+//   };
+
+//   render() {
+//     const { img } = this.props;
+//     return createPortal(
+//       <div className={css.modal_backdrop} onClick={this.handleBackdropClick}>
+//         <div className={css.modal_content}>
+//           <img src={img} alt="" />
+//         </div>
+//       </div>,
+//       modalRoot
+//     );
+//   }
+// }
+
+// Modal.propTypes = {
+//   img: PropTypes.string.isRequired,
+// };

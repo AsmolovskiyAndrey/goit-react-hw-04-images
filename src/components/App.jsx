71025dda1907a7cfee48013de18f9acbddbdfc56
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2023-01-05 13:57:16 +0100

    worked

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,93 +1,179 @@
-import { fetchImages } from 'api/ApiSearch';
-import { Component } from 'react';
+// import { fetchImages } from 'api/ApiSearch';
+import { useState, useEffect } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import css from './App.module.css';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
 import { Loader } from './Loader/Loader';
 import { Modal } from 'components/Modal/Modal';
-export class App extends Component {
-  state = {
-    subject: [],
-    page: 1,
-    search: '',
-    loading: false,
-    notFound: false,
-    error: false,
-    loadMore: false,
-    modal: false,
-    modalImg: '',
-  };
+import axios from 'axios';
 
-  async componentDidUpdate(prevProps, prevState) {
-    const { search, page } = this.state;
-    try {
-      if (prevState.search !== search || prevState.page !== page) {
-        this.setState({ loading: true });
+export const App = () => {
+  const [subject, setSubject] = useState([]);
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [notFound, setNotFound] = useState(false);
+  const [error, setError] = useState(false);
+  const [loadMore, setLoadMore] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [modalImg, setModalImg] = useState('');
 
-        const newSub = await fetchImages(search, page);
-        this.setState(prevState => ({
-          subject: [...prevState.subject, ...newSub],
-        }));
+  useEffect(() => {
+    if (search === '') {
+      return;
+    }
 
-        newSub.length === 12
-          ? this.setState({ loadMore: true })
-          : this.setState({ loadMore: false });
+    const fetchImages = async (search, page) => {
+      axios.defaults.baseURL = 'https://pixabay.com/api';
+      const API_KEY = '31273147-56325c5e652f187dddce9fa62';
 
-        this.setState({ loading: false });
+      try {
+        setLoading(true);
+        const newSub = await axios.get(
+          `/?key=${API_KEY}&per_page=12&q=${search}&page=${page}`
+        );
+
+        setSubject(prevState => [...prevState, ...newSub.data.hits]);
+
+        newSub.data.hits.length === 12 ? setLoadMore(true) : setLoadMore(false);
+        setLoading(false);
 
         if (newSub.length === 0) {
-          this.setState({ notFound: true });
+          setNotFound(true);
         }
+      } catch {
+        setError(true);
       }
-    } catch {
-      this.setState({ error: true });
-    }
-  }
+    };
+    fetchImages(search, page);
+  }, [search, page]);
 
-  searchSubject = event => {
-    const normaliseValue = event.toLowerCase().trim();
-    this.setState({ search: normaliseValue });
-    this.setState({ subject: [] });
-    this.setState({ page: 1 });
-    this.setState({ notFound: false });
-    this.setState({ loadMore: false });
+  const searchSubject = event => {
+    setSearch(event.toLowerCase().trim());
+    setSubject([]);
+    setPage(1);
+    setNotFound(false);
+    setLoadMore(false);
   };
 
-  loadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+  const loadMoreFunc = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  openModal = img => {
-    this.setState({ modalImg: img });
-    this.setState({ modal: true });
+  const openModal = img => {
+    setModalImg(img);
+    setModal(true);
   };
 
-  closeModal = () => {
-    this.setState({ modal: false });
+  const closeModal = () => {
+    setModal(false);
   };
 
-  render() {
-    return (
-      <div className={css.App}>
-        <Searchbar onSubmit={this.searchSubject} />
-        {this.state.loading && <Loader />}
-        {this.state.notFound && <h2>Nothing found for your search...</h2>}
-        {this.state.error && (
-          <h2>Something went wrong, try reloading the page</h2>
-        )}
-        {this.state.subject && (
-          <ImageGallery
-            galleryItems={this.state.subject}
-            onClick={this.openModal}
-          />
-        )}
-        {this.state.loadMore && <Button loadMore={this.loadMore} />}
+  return (
+    <div className={css.App}>
+      <Searchbar onSubmit={searchSubject} />
+      {loading && <Loader />}
+      {notFound && <h2>Nothing found for your search...</h2>}
+      {error && <h2>Something went wrong, try reloading the page</h2>}
+      {subject && <ImageGallery galleryItems={subject} onClick={openModal} />}
+      {loadMore && <Button loadMore={loadMoreFunc} />}
 
-        {this.state.modal && (
-          <Modal img={this.state.modalImg} closeModal={this.closeModal} />
-        )}
-      </div>
-    );
-  }
-}
+      {modal && <Modal img={modalImg} closeModal={closeModal} />}
+    </div>
+  );
+};
+
+// import { fetchImages } from 'api/ApiSearch';
+// import { Component } from 'react';
+// import { Searchbar } from './Searchbar/Searchbar';
+// import css from './App.module.css';
+// import { ImageGallery } from './ImageGallery/ImageGallery';
+// import { Button } from './Button/Button';
+// import { Loader } from './Loader/Loader';
+// import { Modal } from 'components/Modal/Modal';
+// export class App extends Component {
+//   state = {
+//     subject: [],
+//     page: 1,
+//     search: '',
+//     loading: false,
+//     notFound: false,
+//     error: false,
+//     loadMore: false,
+//     modal: false,
+//     modalImg: '',
+//   };
+
+//   async componentDidUpdate(prevProps, prevState) {
+//     const { search, page } = this.state;
+//     try {
+//       if (prevState.search !== search || prevState.page !== page) {
+//         this.setState({ loading: true });
+
+//         const newSub = await fetchImages(search, page);
+//         this.setState(prevState => ({
+//           subject: [...prevState.subject, ...newSub],
+//         }));
+
+//         newSub.length === 12
+//           ? this.setState({ loadMore: true })
+//           : this.setState({ loadMore: false });
+
+//         this.setState({ loading: false });
+
+//         if (newSub.length === 0) {
+//           this.setState({ notFound: true });
+//         }
+//       }
+//     } catch {
+//       this.setState({ error: true });
+//     }
+//   }
+
+//   searchSubject = event => {
+//     const normaliseValue = event.toLowerCase().trim();
+//     this.setState({ search: normaliseValue });
+//     this.setState({ subject: [] });
+//     this.setState({ page: 1 });
+//     this.setState({ notFound: false });
+//     this.setState({ loadMore: false });
+//   };
+
+//   loadMore = () => {
+//     this.setState(prevState => ({ page: prevState.page + 1 }));
+//   };
+
+//   openModal = img => {
+//     this.setState({ modalImg: img });
+//     this.setState({ modal: true });
+//   };
+
+//   closeModal = () => {
+//     this.setState({ modal: false });
+//   };
+
+//   render() {
+//     return (
+//       <div className={css.App}>
+//         <Searchbar onSubmit={this.searchSubject} />
+//         {this.state.loading && <Loader />}
+//         {this.state.notFound && <h2>Nothing found for your search...</h2>}
+//         {this.state.error && (
+//           <h2>Something went wrong, try reloading the page</h2>
+//         )}
+//         {this.state.subject && (
+//           <ImageGallery
+//             galleryItems={this.state.subject}
+//             onClick={this.openModal}
+//           />
+//         )}
+//         {this.state.loadMore && <Button loadMore={this.loadMore} />}
+
+//         {this.state.modal && (
+//           <Modal img={this.state.modalImg} closeModal={this.closeModal} />
+//         )}
+//       </div>
+//     );
+//   }
+// }
